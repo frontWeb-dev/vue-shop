6a3175a003f7f91f2b528c3fa3337cb2ae548171
frontweb-dev <frontend_web@kakao.com>
2022-12-13 16:36:36 +0900

    feat: fontawesome 추가

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,14 @@ import router from './routes';
 import App from './App.vue';
 
 import './assets/styles/tailwind.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faBars);
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.mount('#app');
+
+app.component('font-awesome-icon', FontAwesomeIcon);
