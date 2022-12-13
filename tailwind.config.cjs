5e1953456f2e902212de7298bdd30282674965b8
frontweb-dev <frontend_web@kakao.com>
2022-12-13 16:38:09 +0900

    chore: configure

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,18 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        btnColor: '#191d24',
+      },
+      screens: {
+        sm: '480px',
+        xl: '1360px',
+      },
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.4, 0, 0.2, 1)',
+      },
+    },
   },
   variants: {
     extend: {},
