ac175a42cc911867716cc681d17c394c2515bfe0
frontweb-dev <frontend_web@kakao.com>
2022-12-16 17:14:16 +0900

    feat: 상품 상세정보 페이지

---
+++ src/components/ItemDetail.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="item_detail pt-16" v-for="item in data" :key="item.id">
+    <section class="pt-4 lg:pt-5 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
+      <div class="py-2">
+        {{ menu.filter((a: any) => item.category.match(a.apiCategory))[0].category }} >
+        {{ item.title }}
+      </div>
+      <div class="lg:flex lg:items-center mt-6 md:mt-14 px-2 lg:px-0">
+        <div class="flex-shrink-0 rounded-2xl overflow-hidden px-4 py-4 bg-white">
+          <img class="object-contain w-full h-72" :src="item.image" />
+        </div>
+        <div class="flex flex-col flex-auto p-8 gap-2 px-1 lg:px-12">
+          <h2 class="flex items-center gap-2 text-xl font-bold">
+            {{ item.title }} <span class="badge badge-accent ml-2">NEW</span>
+          </h2>
+          <p>{{ item.description }}</p>
+          <div class="flex items-center mt-3">
+            <p class="ml-2">{{ item.rating.rate }} / {{ item.rating.count }} 참여</p>
+          </div>
+          <p class="mt-2 mb-4 text-3xl">$ {{ item.price }}</p>
+          <div class="flex items-start gap-2">
+            <button class="btn btn-md btn-primary text-white">장바구니에 담기</button>
+            <router-link :to="{ name: 'cart' }" class="btn btn-md btn-border ml-1"
+              >장바구니로 이동</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { menu } from '../mocks/menu';
+import { useApiStore } from '../stores/api';
+
+export default {
+  setup() {
+    const route = useRoute();
+    const store = useApiStore();
+    const data = ref();
+
+    onMounted(async () => {
+      const id = route.params.id.toString();
+      await store.fetchApi();
+
+      data.value = store.filterId(+id);
+      console.log(data.value);
+    });
+    return { data };
+  },
+  data() {
+    return {
+      menu: menu,
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -1,7 +1,15 @@
 <template>
-  <div></div>
+  <Suspense>
+    <ItemDetail />
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
 </template>
 <script lang="ts">
-export default {};
+import ItemDetail from '../components/ItemDetail.vue';
+export default {
+  components: { ItemDetail },
+};
 </script>
 <style></style>
