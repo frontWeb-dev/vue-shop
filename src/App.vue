559c71ed5c252fb103700a2644b235ff92445f5c
frontweb-dev <frontend_web@kakao.com>
2022-12-16 17:15:09 +0900

    chore: watc문 부모 컴포넌트로 옮김

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app">
     <Header />
-    <router-view />
+    <router-view :category="category" />
   </div>
 </template>
 <script lang="ts">
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { menu } from './mocks/menu';
 import Header from './components/Header.vue';
+
 export default {
   name: 'app',
   components: { Header },
+
+  setup() {
+    const route = useRoute();
+    const category = ref();
+
+    watch(
+      () => route.params.category,
+      () => {
+        if (route.params.category !== undefined) {
+          category.value = menu.filter((a) => a.url === route.params.category)[0].category;
+        }
+      }
+    );
+
+    return { category };
+  },
 };
 </script>
 <style></style>
