cfe00b831825ae9daf7a49b3a349fdbef11ef1ef
frontweb-dev <frontend_web@kakao.com>
2022-12-16 17:14:05 +0900

    chore: item List 스타일 수정

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,38 +1,68 @@
 <template>
-  <div class="list">
-    <div v-if="$route.params.category === undefined">
-      <div>{{ category }}</div>
-      <div v-for="product in filterData.slice(0, 4)" :key="product.id">
-        {{ product.title }}
+  <div class="item_List pt-16">
+    <section
+      v-if="$route.params.category === undefined"
+      class="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto"
+    >
+      <h2 class="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">{{ category }}</h2>
+      <div class="grid gap-6 xmd:item_list md:grid-cols-2 lg:grid-cols-4">
+        <ItemCard v-for="product in filterData.slice(0, 4)" :key="product.id" :product="product" />
       </div>
-    </div>
-    <div v-if="$route.params.category !== undefined">
-      <div>{{ category }}</div>
-      <div v-for="product in filterData" :key="product.id">{{ product.title }}</div>
-    </div>
+    </section>
+    <section
+      v-if="$route.params.category !== undefined"
+      class="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto"
+    >
+      <div>홈 > {{ category }}</div>
+      <h2 class="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">{{ category }}</h2>
+      <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <ItemCard v-for="product in filterData" :key="product.id" :product="product" />
+      </div>
+    </section>
   </div>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from '@vue/runtime-core';
+<script lang="ts">
+import { onMounted, watch } from '@vue/runtime-core';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+
 import { menu } from '../mocks/menu';
 import { useApiStore } from '../stores/api';
-import { itemsProps } from '../utils/types';
+import ItemCard from './ItemCard.vue';
 
-const store = useApiStore();
-const props = defineProps<{
-  category: string;
-}>();
-let filterData: itemsProps[] = [];
+export default {
+  props: {
+    category: String,
+  },
+  components: { ItemCard },
 
-onMounted(async () => {
-  store.fetchProducts();
-  const products = store.getProducts();
-  const map = menu.filter((a) => a.category === props.category)[0].apiCategory;
-  filterData = products.filter((a: any) => a.category.match(map));
-});
+  setup(props) {
+    const store = useApiStore();
+    const filterData = ref();
+    const map = ref();
+    const { products } = storeToRefs(store);
 
-console.log(filterData);
+    onMounted(async () => {
+      await store.fetchApi();
+      map.value = menu.filter((a) => a.category === props.category)[0].apiCategory;
+      filterData.value = store.filterDatas(map.value);
+      return { products, filterData };
+    });
+
+    watch(
+      () => props,
+      () => {
+        map.value = menu.filter((a) => a.category === props.category)[0].apiCategory;
+        filterData.value = store.filterDatas(map.value);
+        return { filterData };
+      },
+      { deep: true, immediate: true }
+    );
+
+    return { map, filterData };
+  },
+};
 </script>
 
 <style></style>
--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -6,19 +6,15 @@
     </template>
   </Suspense>
 </template>
+
 <script lang="ts">
 import ItemList from '../components/ItemList.vue';
-import { menu } from '../mocks/menu';
+
 export default {
-  data() {
-    return {
-      menus: menu,
-      products: [],
-      category: menu.filter((a) => a.url === this.$route.params.category)[0].category,
-    };
+  props: {
+    category: String,
   },
 
-  methode: {},
   components: { ItemList },
 };
 </script>
