16ce3ba6eba5ed80377b8c575937ef1cc3da8749
frontweb-dev <frontend_web@kakao.com>
2022-12-20 13:42:56 +0900

    feat: mock 객체 추가

---
+++ src/mocks/card.ts
@@ -0,0 +1,26 @@
+export const cards = [
+  {
+    image: '/assets/asset 21.svg',
+    name: 'visa',
+  },
+  {
+    image: '/assets/asset 22.svg',
+    name: 'marster',
+  },
+  {
+    image: '/assets/asset 23.svg',
+    name: 'amex',
+  },
+  {
+    image: '/assets/asset 24.svg',
+    name: 'cardImage',
+  },
+  {
+    image: '/assets/asset 25.svg',
+    name: 'cardImage',
+  },
+  {
+    image: '/assets/asset 26.svg',
+    name: 'discover',
+  },
+];
---
+++ src/mocks/icons.ts
@@ -0,0 +1,17 @@
+export const icons = [
+  {
+    image: '/assets/asset 27.svg',
+    name: 'Facebook',
+    path: 'https://www.facebook.com/0base',
+  },
+  {
+    image: '/assets/asset 28.svg',
+    name: 'Instagram',
+    path: 'https://www.instagram.com/zerobase.official/',
+  },
+  {
+    image: '/assets/asset 29.svg',
+    name: 'Githubk',
+    path: 'https://github.com/oinochoe',
+  },
+];
